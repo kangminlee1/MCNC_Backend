d74454554d27441b2815e1b8b8b00f0b4a038ab5
GeonHeeLeee <ghlee00125@naver.com>
2024-11-06 15:51:20 +0900

    init: 프로젝트 초기 세팅

---
+++ survwey/survwey/src/main/java/mcnc/survwey/SurvweyApplication.java
@@ -0,0 +1,13 @@
+package mcnc.survwey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvweyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurvweyApplication.class, args);
+	}
+
+}
---
+++ survwey/survwey/src/test/java/mcnc/survwey/SurvweyApplicationTests.java
@@ -0,0 +1,13 @@
+package mcnc.survwey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurvweyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
