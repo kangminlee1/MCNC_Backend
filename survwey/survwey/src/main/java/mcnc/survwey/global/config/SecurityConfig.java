598771d16f16a1a3bdd0954cb2822ec110ec2841
GeonHeeLeee <ghlee00125@naver.com>
2024-11-07 12:43:01 +0900

    feat: 디렉터리 생성 및 securityConfig 작성

---
+++ survwey/survwey/src/main/java/mcnc/survwey/global/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package mcnc.survwey.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // 람다 표현식으로 변경된 방식
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .formLogin(security -> security.disable())
+                .httpBasic(security -> security.disable());
+
+        return http.build();
+    }
+}
+
