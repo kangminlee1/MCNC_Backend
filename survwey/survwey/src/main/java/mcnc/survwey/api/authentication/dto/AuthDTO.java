217051c37d917276a7b8c0099c9e3d5d05ab9418
kangminlee1 <jack7853@naver.com>
2024-11-08 09:32:32 +0900

    feat: 회원가입 테스트 코드 작성

--- survwey/survwey/src/main/java/mcnc/survwey/api/authentication/dto/AuthDTO.java
+++ survwey/survwey/src/main/java/mcnc/survwey/api/authentication/dto/AuthDTO.java
@@ -2,6 +2,7 @@ package mcnc.survwey.api.authentication.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,17 +26,17 @@ public class AuthDTO {
     )
     private String password;
 
-    @NotBlank(message = "생년월일은 필수입니다.")
+    @NotNull(message = "생년월일은 필수입니다.")
 //    @Pattern(
 //            regexp = "^\\d{4}-\\d{2}-\\d{2}$",
 //            message = "생년월일은 yyyy-MM-dd 형식이어야 합니다.")front랑 맞추자
     private LocalDate birth;
 
-    @NotBlank(message = "성별은 필수입니다.")
-    @Pattern(
-            regexp = "^[MF]$\n",
-            message = "성별은 M, F만 포함해야 합니다."
-    )
+    @NotNull(message = "성별은 필수입니다.")
+//    @Pattern(
+//            regexp = "^[MF]$\n",
+//            message = "성별은 M, F만 포함해야 합니다."
+//    )
     private Gender gender;
 
     @NotBlank(message = "이름은 필수입니다.")
